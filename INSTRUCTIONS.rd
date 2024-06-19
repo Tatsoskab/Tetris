27961c047858ebd2c5a146fa6b4696faf6ea5ed1
Anaskamperos <tatsoskab@gmail.com>
2024-06-19 20:31:45 +0300

    added collision

---
+++ INSTRUCTIONS.rd
@@ -0,0 +1,4 @@
+A: to move the tetramino to the left
+D: to move the tetramino to the right 
+S: to accelerate towards the bottom
+W: to turn the tetramino 90 degrees 
--- tetris.py
+++ tetris.py
@@ -1,4 +1,6 @@
 import pygame
+from copy import deepcopy
+from random import choice, randrange
 W, H = 10, 20
 TILE = 45
 GAME_RES = W * TILE, H * TILE
@@ -22,7 +24,27 @@ figures = [[pygame.Rect(x + W // 2, y + 1, 1, 1) for x, y in fig_pos] for fig_po
 
 figure_rect = pygame.Rect(0, 0, TILE - 2, TILE - 2)
 
-figure = figures[0]
+field = [[0 for i in range(W)] for j in range(H)]
+
+
+
+
+
+
+
+anim_count, anim_speed, anim_limit = 0, 30, 2000
+
+
+
+figure = deepcopy(choice(figures))
+
+
+def check_borders():
+    if figure[i].x < 0 or figure[i].x > W - 1:
+        return False
+    elif figure[i].y > H - 1 or field[figure[i].y][figure[i].x]:
+        return False
+    return True
 
 
 while True:
@@ -36,19 +58,59 @@ while True:
                 dx -= 1
             elif event.key == pygame.K_d:
                 dx += 1
+            elif event.key == pygame.K_s:
+                anim_limit = 100
 
 
-    [pygame.draw.rect((window),(40,40,40),i_rect,1,) for i_rect in grid]
-
+    figure_old = deepcopy(figure)
     for i in range(4):
         figure[i].x += dx
+        if not check_borders():
+            figure = deepcopy(figure_old)
+            break
+
+
+
+
+
+
+    for i in range(4):
         figure_rect.x = figure[i].x * TILE
         figure_rect.y = figure[i].y * TILE
-
         pygame.draw.rect(window,pygame.Color("white"),figure_rect)
 
 
 
 
+    anim_count += anim_speed
+
+
+    if anim_count > anim_limit:
+        anim_count = 0
+        figure_old = deepcopy(figure)
+        for i in range(4):
+            figure[i].y += 1
+            if not check_borders():
+                for i in range(4):
+                    field[figure_old[i].y][figure_old[i].x] = pygame.Color("white")
+                figure = deepcopy(choice(figures))
+                anim_limit = 2000
+                break
+
+
+
+ 
+
+    [pygame.draw.rect((window),(40,40,40),i_rect,1,) for i_rect in grid]
+
+
+
+
+    for y, raw in enumerate(field):
+        for x, col in enumerate(raw):
+            if col:
+                figure_rect.x = x * TILE
+                figure_rect.y = y * TILE
+                pygame.draw.rect(window,col,figure_rect)
     pygame.display.update()
     clock.tick()
